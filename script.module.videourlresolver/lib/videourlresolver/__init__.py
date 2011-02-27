3a6850d5aba35752f05a02f7e9e1d52b9b0a286d
anarchintosh <j48antialias@gmail.com>
2011-02-27 13:22:13 +0000

    mock-up of the folder/file structure

--- script.module.videourlresolver/lib/videourlresolver/__init__.py
+++ script.module.videourlresolver/lib/videourlresolver/__init__.py
@@ -1,1 +1,3 @@
 
+# a place for the module to store login cookies would need to be defined…
+# use userdata/addon_data?
---
+++ script.module.videourlresolver/lib/videourlresolver/autoresolve.py
@@ -0,0 +1,4 @@
+
+# detector that checks what resolver to use for url
+
+# ie.   re.search('.megaupload.com/', url)
